04d5845be5116b389f956a638a25b8cee2f736fe
Maximilian Ammann <max@maxammann.org>
2019-05-12 16:02:38 +0200

    Add debug statements

--- DeWave/clip.py
+++ DeWave/clip.py
@@ -12,17 +12,22 @@ def audio_clip(data_dir, N, low, high, duration, output_dir):
 
     print("Speaker count: %s" % len(speakers_dirs))
     for i in range(len(speakers_dirs)):
-        print("Clipping from speaker %d: %s" % (i, len(speakers_dirs)))
         speaker_dir = speakers_dirs[i]
+        print("Speaker:\t%d" % i)
+        print("Source:\t%s" % speaker_dir)
+        
         audio_files = glob.glob(os.path.join(speaker_dir, "**/*.sph"), recursive=True)
         audio_files.extend(glob.glob(os.path.join(speaker_dir, "**/*.wav"), recursive=True))
         audio_files.extend(glob.glob(os.path.join(speaker_dir, "**/*.m4a"), recursive=True))
+
+        print("Source files:\t%d" % len(audio_files))
 
         p = os.path.join(output_dir, str(i))
         if not os.path.exists(p):
             os.makedirs(p)
         
         for audio_file in audio_files:
+            print("\t%s" % audio_file)
             y, _ = librosa.load(audio_file, sr=SAMPLING_RATE)
             for j in range(N):
                 k = int(np.random.uniform(low,  min(high, (len(y) - duration*SAMPLING_RATE) / SAMPLING_RATE), size=1))

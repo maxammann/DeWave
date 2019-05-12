b9c6956c52c9b6ead181dbeaee8be56a6a42c47d
Maximilian Ammann <max@maxammann.org>
2019-05-12 15:33:14 +0200

    Speaker files do no longer need to be a single file

--- DeWave/clip.py
+++ DeWave/clip.py
@@ -6,15 +6,23 @@ from .constant import *
 import argparse
 
 def audio_clip(data_dir, N, low, high, duration, output_dir):
-    speakers = glob.glob(os.path.join(data_dir, "**/*.sph"), recursive=True)
-    speakers.extend(glob.glob(os.path.join(data_dir, "**/*.wav"), recursive=True))
-    speakers.extend(glob.glob(os.path.join(data_dir, "**/*.m4a"), recursive=True))
-    for i in range(len(speakers)):
+    speakers_dirs = [os.path.join(data_dir, i) for i in os.listdir(data_dir)\
+                        if os.path.isdir(os.path.join(data_dir, i))]
+
+    print("Speaker count: %s" % len(speakers_dirs))
+    for i in range(len(speakers_dirs)):
+        speaker_dir = speakers_dirs[i]
+        audio_files = glob.glob(os.path.join(speaker_dir, "**/*.sph"), recursive=True)
+        audio_files.extend(glob.glob(os.path.join(speaker_dir, "**/*.wav"), recursive=True))
+        audio_files.extend(glob.glob(os.path.join(speaker_dir, "**/*.m4a"), recursive=True))
+
         p = os.path.join(output_dir, str(i))
         if not os.path.exists(p):
             os.makedirs(p)
-        y, _ = librosa.load(speakers[i], sr=SAMPLING_RATE)
-        for j in range(N):
-            k = int(np.random.randint(low,  min(high, (len(y) - duration*SAMPLING_RATE) / SAMPLING_RATE), size=1))
-            librosa.output.write_wav(os.path.join(p, str(j)) + ".wav", 
-              y[k*SAMPLING_RATE : (k+duration)*SAMPLING_RATE], SAMPLING_RATE)
+        
+        for audio_file in audio_files:
+            y, _ = librosa.load(audio_file, sr=SAMPLING_RATE)
+            for j in range(N):
+                k = int(np.random.randint(low,  min(high, (len(y) - duration*SAMPLING_RATE) / SAMPLING_RATE), size=1))
+                librosa.output.write_wav(os.path.join(p, str(j)) + ".wav", 
+                y[k*SAMPLING_RATE : (k+duration)*SAMPLING_RATE], SAMPLING_RATE)

ec0357157a256d6033f2895b6161d3c16dc34267
Maximilian Ammann <max@maxammann.org>
2019-05-17 17:09:14 +0200

    Disable dropouts like in the original paper

--- DeWave/constant.py
+++ DeWave/constant.py
@@ -17,9 +17,9 @@ EMBBEDDING_D = 40
 GLOBAL_MEAN = 44
 GLOBAL_STD = 15.5
 # feed forward dropout prob
-P_DROPOUT_FF = 0.5
+P_DROPOUT_FF = 1
 # recurrent dropout prob
-P_DROPOUT_RC = 0.2
+P_DROPOUT_RC = 1
 N_HIDDEN = 600
 LEARNING_RATE = 1e-5
 MAX_STEP = 2000000
